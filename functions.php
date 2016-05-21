6b173edc15e967b2562c2bd03e23e41248927726
wkwkrnht <wkwkrnht@gmail.com>
2016-05-21 21:57:21 +0900

    add

---
+++ functions.php
@@ -0,0 +1,25 @@
+<? php
+function wkwkrnht_setup(){
+    add_theme_support('post-formats',array('aside','gallery','quote','image','link','status','video','audio','chat'));
+    add_theme_support('post-thumbnails');
+    add_theme_support('custom-background');
+    add_theme_support( 'custom-logo',array('height'=>248,'width'=>248,'flex-height'=>true,));
+    add_theme_support('custom-header');
+    add_theme_support('title-tag');
+    add_theme_support('automatic-feed-links');
+    add_theme_support('html5',array('comment-list','comment-form','search-form','gallery','caption'));
+    if(!isset($content_width)):$content_width='100vw';endif;
+    register_nav_menu('main','main-menu');
+    register_nav_menu('social','social-menu');
+}
+add_action('after_setup_theme','wkwkrnht_setup');
+function wpdocs_theme_add_editor_styles(){
+    add_editor_style('css/custom-editor-style.css');
+}
+add_action('admin_init','wpdocs_theme_add_editor_styles');
+function theme_slug_widgets_init(){
+    register_sidebar(array('name'=>__('Main Sidebar','theme-slug'),'description'=>__('Widgets in this area will be shown on all posts and pages.','theme-slug'),'id'=>'floatmenu','before_widget'=>'<li id="%1$s" class="widget %2$s">','after_widget'=>'</li>','before_title'=>'<h2 class="widget-title">','after_title' =>'</h2>',));
+}
+add_action('widgets_init','wkwkrnht_sidebar_widgets_init');
+remove_action('wp_head','print_emoji_detection_script',7);
+remove_action('wp_print_styles','print_emoji_styles');
