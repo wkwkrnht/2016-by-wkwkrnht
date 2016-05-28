02f6fc80b4833b5bbfddf5377acf59a2a2247435
wkwkrnht <wkwkrnht@gmail.com>
2016-05-28 18:06:42 +0900

    fix

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-<?php $amp=false;$string=$post->post_content;$nowurl=$_SERVER["REQUEST_URI"];if(strpos($nowurl,'amp')!==false){$amp=true;}?>
-<?php if($amp):?>
-	<?php include(get_template_directory() . '/amp.php');?>
-<?php else:?>
-    <?php get_header();?>
-		<?php wkwkrnht_special_card();?>
-        <?php if(have_posts()):while(have_posts()):the_post();get_template_part('card-list',get_post_format())endwhile;endif;?>
-		<?php wp_link_pages(array('before'=>'<div class="page-links">','after'=>'</div>','link_before'=>'<span>','link_after' =>'</span>',));?>
-    <?php get_footer();?>
-<?php endif;?>
+<?php get_header();?>
+	<?php wkwkrnht_special_card();?>
+	<div class="article-list">
+    	<?php if(have_posts()):while(have_posts()):the_post();get_template_part('card-list',get_post_format())endwhile;endif;?>
+	</div>
+	<div class="page-links">
+		<?php wp_link_pages(array('link_before'=>'<span>','link_after' =>'</span>',));?>
+	</div>
+<?php get_footer();?>
--- parts/card-list.php
+++ parts/card-list.php
@@ -1,9 +1,7 @@
-<div class="article-list">
-    <section class="card article-card">
-        <a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>" class="article-img"><?php if(has_post_thumbnail()):the_post_thumbnail();else:echo'<img src="../img/no-img.png" alt="eyecatch">'endif;?></a>
-        <div class="article-info">
-            <h2 class="article-name">01</h2><br>
-            <span class="article-meta"><?php the_author();the_time();the_category(', ');?></span>
-        </div>
-    </section>
-</div>
+<section class="card article-card">
+    <a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>" class="article-img"><?php if(has_post_thumbnail()):the_post_thumbnail();else:echo'<img src="../img/no-img.png" alt="eyecatch">'endif;?></a>
+    <div class="article-info">
+        <h2 class="article-name"><?php the_title();?></h2><br>
+        <span class="article-meta"><?php the_author();the_time();the_category(', ');?></span>
+    </div>
+</section>
