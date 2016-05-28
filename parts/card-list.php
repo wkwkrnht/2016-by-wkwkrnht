cd775a1916929b8b51f04741e4dba0608e751c77
wkwkrnht <wkwkrnht@gmail.com>
2016-05-28 20:19:57 +0900

    test

--- parts/card-list.php
+++ parts/card-list.php
@@ -1,7 +1,7 @@
 <section class="card article-card">
-    <a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>" class="article-img"><?php if(has_post_thumbnail()):the_post_thumbnail();else:echo'<img src="../img/no-img.png" alt="eyecatch">'endif;?></a>
+    <a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>" class="article-eye"><?php if(has_post_thumbnail()):the_post_thumbnail();else:echo'<img src="' . get_stylesheet_directory_uri() . '/img/no-img.png" alt="eyecatch">'endif;?></a>
     <div class="article-info">
         <h2 class="article-name"><?php the_title();?></h2><br>
-        <span class="article-meta"><?php the_author();the_time();the_category(', ');?></span>
+        <span class="article-meta"><?php the_author();echo'<time>';the_time();echo'</time>';the_category(', ');?></span>
     </div>
 </section>
