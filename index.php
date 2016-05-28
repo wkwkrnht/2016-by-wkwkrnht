ab2c7ad765ad04aae96b0aa4f60d02516a25abb4
wkwkrnht <wkwkrnht@gmail.com>
2016-05-28 20:05:29 +0900

    fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header();?>
 	<?php wkwkrnht_special_card();?>
 	<div class="article-list">
-    	<?php if(have_posts()):while(have_posts()):get_template_part('parts/card-list',get_post_format());endwhile;endif;?>
+    	<?php if(have_posts()):while(have_posts()):the_post();get_template_part('parts/card-list',get_post_format());endwhile;endif;?>
 	</div>
 	<div class="page-links">
 		<?php wp_link_pages(array('link_before'=>'<span>','link_after' =>'</span>',));?>
