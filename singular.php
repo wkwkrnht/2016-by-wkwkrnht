75c3d2a829e5ec5955cb58e6b076acbea8be4519
wkwkrnht <wkwkrnht@gmail.com>
2016-05-29 00:30:38 +0900

    fix

--- singular.php
+++ singular.php
@@ -1,4 +1,5 @@
 <?php get_header();?>
+<?php if(have_posts()):while(have_posts()):?>
 	<article id="post-<?php the_ID();?>" <?php post_class();?>>
 		<header class="article-header">
 			<div class="article-img"><?php if(has_post_thumbnail()):the_post_thumbnail();else:echo'<img src="' . get_stylesheet_directory_uri() . '/img/no-img.png" alt="eyecatch">'endif;?></div>
@@ -9,6 +10,6 @@
 			<?php the_content();?>
 		</div>
 		<?php the_tags('<ul class="article-tag"><li>','</li><li>','</li></ul>');?>
-		<?php comments_template();?>
 	</article>
+<?php endwhile;endif;?>
 <?php get_footer();?>
