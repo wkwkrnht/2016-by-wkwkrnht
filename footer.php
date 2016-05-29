f5d95f11ae58a841b6f01eec4fc92258c8d680ba
wkwkrnht <wkwkrnht@gmail.com>
2016-05-29 12:20:45 +0900

    test

--- footer.php
+++ footer.php
@@ -1,4 +1,9 @@
     </main>
+    <div class="toggle-zone">
+        <span id="share-toggle"><i class="fa fa-share-alt" aria-hidden="true"></i></span>
+        <span id="menu-toggle"><i class="fa fa-bars" aria-hidden="true"></i></span>
+        <span id="share-toggle"><i class="fa fa-share-alt" aria-hidden="true"></i></span>
+    </div>
     <nav id="share-menu">
         <ui>
 		    <li><a class="twitter"></a></li>
@@ -35,14 +40,10 @@
             </ul>
         <?php endif;?>
     </div>
-    <div class="toggle-zone">
-        <input id="share-check" type="checkbox" value="off"><label id="share-toggle" for="share-check"><i class="fa fa-share-alt" aria-hidden="true"></i></label>
-        <input id="main-check" type="checkbox" value="off"><label id="menu-toggle" for="main-check"><i class="fa fa-bars" aria-hidden="true"></i></label>
-        <input id="share-check" type="checkbox" value="off"><label id="share-toggle" for="share-check"><i class="fa fa-share-alt" aria-hidden="true"></i></label>
-    </div>
     <script>
-        jquery(function(){function tableData(){var index ='';var headTxt ='';$('.article-main table').each(function(){$(this).find('thead tr th').each(function(){index = $(this).index()-1;headTxt = $(this).text();$(this).parents('table').find('tbody tr').each(function(){$(this).find('td').eq(index).attr('data-th',headTxt);});});});}tableData();});
-        document.body.addEventListener("click"s drop,false);function drop(e){var x = e.pageX;var y = e.pageY;var sizuku = document.createElement("div");sizuku.style.top = y + "px";sizuku.style.left = x + "px";document.body.appendChild(sizuku);sizuku.className = "sizuku";sizuku.addEventListener("animationend",function(){this.parentNode.removeChild(this);},false);}
+        jQuery(function(){var $body = jQuery('body');$('#share-toggle').on('click',function(){$body.toggleClass('open');});});
+        jQuery(function(){function tableData(){var index ='';var headTxt ='';$('.article-main table').each(function(){$(this).find('thead tr th').each(function(){index = $(this).index()-1;headTxt = $(this).text();$(this).parents('table').find('tbody tr').each(function(){$(this).find('td').eq(index).attr('data-th',headTxt);});});});}tableData();});
+        document.body.addEventListener("click",drop,false);function drop(e){var x = e.pageX;var y = e.pageY;var sizuku = document.createElement("div");sizuku.style.top = y + "px";sizuku.style.left = x + "px";document.body.appendChild(sizuku);sizuku.className = "sizuku";sizuku.addEventListener("animationend",function(){this.parentNode.removeChild(this);},false);}
 	</script>
     <?php wp_footer();?>
 </body>
