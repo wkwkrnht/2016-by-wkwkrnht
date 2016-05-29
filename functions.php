4f089029ded24243d1990c6a8d8e119ed1d35091
wkwkrnht <wkwkrnht@gmail.com>
2016-05-29 22:23:04 +0900

    fix

--- functions.php
+++ functions.php
@@ -27,7 +27,7 @@ remove_action('wp_print_styles','print_emoji_styles');
 add_filter('body_class','add_body_class');
 function add_body_class($classes){if(!is_singular()):$classes[] = 'card-list';return $classes;endif;}
 add_action('wp_enqueue_scripts','theme_enqueue_scripts_styles');
-function theme_enqueue_scripts_styles()
+function theme_enqueue_scripts_styles(){
     wp_deregister_script('jquery');
     wp_register_script('jquery','');
     wp_enqueue_script('jquery',false,array(),null,true);
