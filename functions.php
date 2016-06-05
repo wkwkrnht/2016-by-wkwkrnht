c7e913e3014eb510e5f70073d8d6b3e56362af2c
wkwkrnht <wkwkrnht@gmail.com>
2016-06-05 18:11:17 +0900

    fix

--- functions.php
+++ functions.php
@@ -111,8 +111,8 @@ function wkwkrnht_special_card(){
     ●1つ進む
     ●最後尾へ
 */
-function pagination($pages='',$range=3){
-    $showitems = ($range * 2)+1;
+function pagenation($pages='',$range=3){
+    $showitems=($range * 2)+1;
     global $paged;
     if(empty($paged)){$paged=1;}
     if($pages==''){global $wp_query;$pages=$wp_query->max_num_pages;if(!$pages){$pages=1;}}
@@ -120,9 +120,9 @@ function pagination($pages='',$range=3){
         echo'<div class="pagination">';
         if($paged>2&&$paged>$range+1&&$showitems<$pages){echo'<a href="'.get_pagenum_link(1).'"><i class="fa fa-angle-double-left" aria-hidden="true"></i></a>';}
         if($paged>1&&$showitems<$pages){echo'<a href="'.get_pagenum_link($paged - 1).'"><i class="fa fa-angle-left" aria-hidden="true"></i></a>';}
-        for($i=1;$i<=$pages;$i++){if(1!=$pages&&(!($i>=$paged+$range+1||$i<=$paged-$range-1)||$pages<=$showitems)){echo ($paged == $i)? '<span class="current">'.$i.'</span>":"<a href="'.get_pagenum_link($i).'" class="inactive">'.$i.'</a>';}}
-        if($paged<$pages&&$showitems<$pages) echo'<a href="'.get_pagenum_link($paged + 1).'"><i class="fa fa-angle-right" aria-hidden="true"></i></a>';
-        if($paged<$pages-1&&$paged+$range-1<$pages && $showitems < $pages) echo'<a href="'.get_pagenum_link($pages).'"><i class="fa fa-angle-double-right" aria-hidden="true"></i></a>';
+        for($i=1;$i<=$pages;$i++){if(1!=$pages&&(!($i>=$paged+$range+1||$i<=$paged-$range-1)||$pages<=$showitems)){echo ($paged==$i)? '<span class="current">'.$i.'</span>':'<a href="'.get_pagenum_link($i).'" class="inactive">'.$i.'</a>';}}
+        if($paged<$pages&&$showitems<$pages){echo'<a href="'.get_pagenum_link($paged + 1).'"><i class="fa fa-angle-right" aria-hidden="true"></i></a>';}
+        if($paged<$pages-1&&$paged+$range-1<$pages&&$showitems<$pages){echo'<a href="'.get_pagenum_link($pages).'"><i class="fa fa-angle-double-right" aria-hidden="true"></i></a>';}
         echo'</div>';
     }
 }
