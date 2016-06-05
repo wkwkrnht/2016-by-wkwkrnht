b08b9ba309da66f3ce0c9a1f30141c6f3dbf5abb
wkwkrnht <wkwkrnht@gmail.com>
2016-06-05 18:46:19 +0900

    fix

--- functions.php
+++ functions.php
@@ -117,12 +117,12 @@ function pagenation($pages='',$range=3){
     if(empty($paged)){$paged=1;}
     if($pages==''){global $wp_query;$pages=$wp_query->max_num_pages;if(!$pages){$pages=1;}}
     if(1!=$pages){
-        echo'<div class="pagenation"><ul>';
+        echo'<ul class="pagenation">';
         if($paged>2&&$paged>$range+1&&$showitems<$pages){echo'<li><a href="'.get_pagenum_link(1).'"><i class="fa fa-angle-double-left" aria-hidden="true"></i></a</li>>';}
         if($paged>1&&$showitems<$pages){echo'<li><a href="'.get_pagenum_link($paged - 1).'"><i class="fa fa-angle-left" aria-hidden="true"></i></a></li>';}
         for($i=1;$i<=$pages;$i++){if(1!=$pages&&(!($i>=$paged+$range+1||$i<=$paged-$range-1)||$pages<=$showitems)){echo ($paged==$i)? '<li><span class="current">'.$i.'</span></li>' : '<li><a href="'.get_pagenum_link($i).'" class="inactive">'.$i.'</a></li>';}}
         if($paged<$pages&&$showitems<$pages){echo'<li><a href="'.get_pagenum_link($paged + 1).'"><i class="fa fa-angle-right" aria-hidden="true"></i></a></li>';}
         if($paged<$pages-1&&$paged+$range-1<$pages&&$showitems<$pages){echo'<li><a href="'.get_pagenum_link($pages).'"><i class="fa fa-angle-double-right" aria-hidden="true"></i></a></li>';}
-        echo'</ul></div>';
+        echo'</ul>';
     }
 }
