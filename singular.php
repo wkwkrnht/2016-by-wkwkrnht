596b82f710308f5d59b090e18dbd617031c67b77
wkwkrnht <wkwkrnht@gmail.com>
2016-06-12 22:54:20 +0900

    test

--- singular.php
+++ singular.php
@@ -1,9 +1,9 @@
 <?php get_header();?>
+<div class="article-img"><?php if(has_post_thumbnail()):the_post_thumbnail();else:echo'<img src="' . get_stylesheet_directory_uri() . '/img/no-img.png" alt="eyecatch">';endif;?></div>
 <article id="post-<?php the_ID();?>" <?php post_class();?>>
-	<header class="article-header">
-		<div class="article-img"><?php if(has_post_thumbnail()):the_post_thumbnail();else:echo'<img src="' . get_stylesheet_directory_uri() . '/img/no-img.png" alt="eyecatch">';endif;?></div>
+	<header class="article-meta">
 		<h2 class="article-name"><?php the_title();?></h2>
-		<span class="article-meta"><?php the_author();?>公開日：<time class="entry-date" datetime="<?php the_time('c');?>"><?php the_time('Y/n/j G:i.s');?></time><?php if($mtime = get_mtime('Y/n/j G:i.s')):echo'最終更新日：' . $mtime;endif;the_category(', ');?></span>
+		<span class="article-info"><?php the_author();?>公開日：<time class="entry-date" datetime="<?php the_time('c');?>"><?php the_time('Y/n/j G:i.s');?></time><?php if($mtime = get_mtime('Y/n/j G:i.s')):echo'最終更新日：' . $mtime;endif;the_category(', ');?></span>
 	</header>
 	<div class="article-main">
 		<?php if(have_posts()):while(have_posts()):the_post();the_content();endwhile;endif;?>
