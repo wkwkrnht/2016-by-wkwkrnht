173fcaa3235cd47fe05038cc6b66ed146deead0c
wkwkrnht <wkwkrnht@gmail.com>
2016-07-02 21:34:18 +0900

    fix

--- singular.php
+++ singular.php
@@ -1,9 +1,9 @@
 <?php get_header();?>
 <article id="post-<?php the_ID();?>" <?php post_class();?>>
 	<header class="article-header">
-		<a href="<?php home_url();?>" class="article-img" style="display:block;background:rgba(0,0,0,.4) url(<?php wkwkrnht_eyecatch();?>) no-repeat;">
+		<a href="<?php home_url();?>" class="article-img" style="display:block;background:url(<?php wkwkrnht_eyecatch();?>) no-repeat center/cover;">
 			<?php bloginfo('name');?>
-		</div>
+		</a>
 		<div class="article-meta">
 			<time class="article-date" datetime="<?php the_time('Y/n/j G:i.s');?>"><?php the_time('Y/n/j');?></time>
 			<span class="article-info"><h2 class="article-name"><?php the_title();?></h2><br><?php the_author();?><?php if($mtime===get_mtime('Y/n/j G:i.s')):echo'最終更新日：' . $mtime;endif;the_category(', ');?></span>
