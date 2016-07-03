aa89539ac5b5f043005db8e1031f74af0b68985e
wkwkrnht <wkwkrnht@gmail.com>
2016-07-03 11:12:15 +0900

    fix

--- singular.php
+++ singular.php
@@ -11,13 +11,13 @@
 		<?php
 		$cat = is_single() ? get_the_category() : array(get_category($cat));
 		if($cat && !is_wp_error($cat)){
-			$par = get_category($cat[0]->parent);
-			echo '<div class="bread" itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a href="'.get_bloginfo('url').'" itemprop="url"><span itemprop="title">ホーム</span></a><span class="sp">/</span></div>';
-			while($par && !is_wp_error($par) && $par->term_id != 0){
-				$echo = '<div itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a href="'.get_category_link($par->term_id).'" itemprop="url"><span itemprop="title">'.$par->name.'</span></a><span class="sp">/</span></div>'.$echo;
-				$par = get_category($par->parent);
+			$par=get_category($cat[0]->parent);$echo='';
+			echo'<div class="bread" itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a href="' . get_bloginfo('url') . '" itemprop="url"><span itemprop="title">ホーム</span></a><span class="sp">/</span></div>';
+			while($par && !is_wp_error($par) && $par->term_id!==0){
+				$echo='<div itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a href="' . get_category_link($par->term_id) . '" itemprop="url"><span itemprop="title">' . $par->name . '</span></a><span class="sp">/</span></div>' . $echo;
+				$par=get_category($par->parent);
 			}
-			echo $echo.'<div itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a href="'.get_category_link($cat[0]->term_id).'" itemprop="url"><span itemprop="title">'.$cat[0]->name.'</span></a></div>';
+			echo $echo . '<div itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a href="'.get_category_link($cat[0]->term_id).'" itemprop="url"><span itemprop="title">' . $cat[0]->name . '</span></a></div>';
 		}
 		?>
 	</header>
