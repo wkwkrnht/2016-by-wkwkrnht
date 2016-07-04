3b5e9940d57001c6550fd5506b659c4fa6405b93
wkwkrnht <wkwkrnht@gmail.com>
2016-07-04 23:14:21 +0900

    fix

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 	<div class="article-list">
     	<?php if(have_posts()):while(have_posts()):the_post();?>
 			<section class="card article-card">
-		        <a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>" class="article-eye"><?php if(has_post_thumbnail()):the_post_thumbnail();else:echo'<img src="' . get_stylesheet_directory_uri() . '/img/no-img.png" alt="eyecatch">';endif;?></a>
-		        <div class="article-info">
-		            <a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>" class="article-eye"><h2 class="article-name"><?php the_title();?></h2></a><br>
-		            <span class="article-meta"><?php the_author();?>公開日：<time class="entry-date" datetime="<?php the_time('c');?>"><?php the_time('Y/n/j G:i.s');?></time><?php if($mtime = get_mtime('Y/n/j G:i.s')):echo'最終更新日：' . $mtime;endif;the_category(', ');?></span>
+		        <a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>" class="article-eye"><img src="<?php wkwkrnht_eyecatch();?>" alt="eyecatch"></a>
+		        <div class="card-info">
+		            <a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>" class="card-eyecatch"><h2 class="article-name"><?php the_title();?></h2></a><br>
+		            <span class="card-meta">公開日：<time class="entry-date" datetime="<?php echo get_mtime('Y/n/j G:i.s');?>"><?php the_time('Y/n/j G:i.s');?></time><?php the_author();the_category(', ');?></span>
 		        </div>
 		    </section>
 		<?php endwhile;endif;?>
