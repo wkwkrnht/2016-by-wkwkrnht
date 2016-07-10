d50228aa4781bf03c59b707d122caa97d68352bd
wkwkrnht <wkwkrnht@gmail.com>
2016-07-10 19:06:46 +0900

    fix

--- widget/post-nav.php
+++ widget/post-nav.php
@@ -1,6 +1,7 @@
 <style>
     .post-nav{height:20vh;width:80vw;}
     .post-nav a{display:inline-block;position:relative;height:10vh;width:80vw;color:#fff;}
+    .post-nav a::before{display:block;content:'';height:10vh;width:80vw;position:absolute;top:0;left:0;background-color:rgba(0,0,0,.3);box-shadow:inset 0 0 50px rgba(0,0,0,.4);}
     <?php wkwkrnht_post_nav_background();?>
 </style>
  <nav class="post-nav">
