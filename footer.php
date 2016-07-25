e56cc38f16ba7269348c1f9d9f3e35d3cc991d56
wkwkrnht <wkwkrnht@gmail.com>
2016-07-25 21:15:40 +0900

    fix

--- footer.php
+++ footer.php
@@ -51,7 +51,7 @@
     <?php //require(get_template_directory() . '/js/script.php');?>
     <script>
         document.body.addEventListener("click",drop,false);function drop(e){var x = e.pageX;var y = e.pageY;var sizuku = document.createElement("div");sizuku.style.top = y + "px";sizuku.style.left = x + "px";document.body.appendChild(sizuku);sizuku.className = "sizuku";sizuku.addEventListener("animationend",function(){this.parentNode.removeChild(this);},false);};
-        jQuery(function(){jQuery('#menu-toggle').click(function(){jQuery('#main-menu').toggleClass('close');jQuery('#main-menu').toggleClass('open');});});
+        jQuery(function(){$('#menu-toggle').click(function(){jQuery('#main-menu').toggleClass('close');$('#main-menu').toggleClass('open');});});
         jQuery(function(){jQuery('#share-toggle').click(function(){jQuery('#share-menu').toggleClass('close');jQuery('#share-menu').toggleClass('open');});});
         jQuery(function(){jQuery('#share-menu-toggle').click(function(){jQuery('#share-menu').toggleClass('close');jQuery('#share-menu').toggleClass('open');});});
     </script>
