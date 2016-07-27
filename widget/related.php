dfe76ea28e368deb0b0492a65dc2bd4d7f60c912
wkwkrnht <wkwkrnht@gmail.com>
2016-07-27 22:09:47 +0900

    fix

--- widget/related.php
+++ widget/related.php
@@ -1,10 +1,25 @@
-<style>
-	#flex{display:flex;flex-wrap:nowrap;justify-content:space-between;align-items:center;height:calc(20vw + 12vmin);width:100%;margin:2vmin 0;overflow-x:auto;overflow-y:hidden;-webkit-overflow-scrolling:touch;}
-	#flex > * {-webkit-transform:translateZ(0px);}
-	.related-wrapper{display:block;height:calc(20vw + 10vmin);width:35vw;border-radius:2vmin;margin:1vmin 3vmin;background-color:#fff;box-shadow:0 0 1vmin rgba(0,0,0,.3);text-align:center;}
-	.related-thumb{height:20vw;width:35vw;background-color:#ffcc00;}
-	.related-title{height:10vmin;font-size:1.8rem;color:#333;text-decoration:none;}
-</style>
+<?php $myAmp=false;$string=$post->post_content;$nowurl=$_SERVER["REQUEST_URI"];if(strpos($nowurl,'amp')!==false&&strpos($string,'<script>')===false&&is_single()){$myAmp=true;};
+if($myAmp===true):
+	echo <<<STYLE
+	<style amp-custom>
+		#flex{display:flex;flex-wrap:nowrap;justify-content:space-between;align-items:center;height:calc(20vw + 12vmin);width:100%;margin:2vmin 0;overflow-x:auto;overflow-y:hidden;-webkit-overflow-scrolling:touch;}
+		#flex > * {-webkit-transform:translateZ(0px);}
+		.related-wrapper{display:block;height:calc(20vw + 10vmin);width:35vw;border-radius:2vmin;margin:1vmin 3vmin;background-color:#fff;box-shadow:0 0 1vmin rgba(0,0,0,.3);text-align:center;}
+		.related-thumb{height:20vw;width:35vw;background-color:#ffcc00;}
+		.related-title{height:10vmin;font-size:1.8rem;color:#333;text-decoration:none;}
+	</style>
+	STYLE;
+else:
+	echo <<<STYLE
+	<style>
+		#flex{display:flex;flex-wrap:nowrap;justify-content:space-between;align-items:center;height:calc(20vw + 12vmin);width:100%;margin:2vmin 0;overflow-x:auto;overflow-y:hidden;-webkit-overflow-scrolling:touch;}
+		#flex > * {-webkit-transform:translateZ(0px);}
+		.related-wrapper{display:block;height:calc(20vw + 10vmin);width:35vw;border-radius:2vmin;margin:1vmin 3vmin;background-color:#fff;box-shadow:0 0 1vmin rgba(0,0,0,.3);text-align:center;}
+		.related-thumb{height:20vw;width:35vw;background-color:#ffcc00;}
+		.related-title{height:10vmin;font-size:1.8rem;color:#333;text-decoration:none;}
+	</style>
+	STYLE;
+endif;?>
 <div id="flex">
 	<?php $categories=get_the_category();$category_ID=array();
 	foreach($categories as $category):array_push($category_ID,$category->cat_ID);endforeach;
