00aa4b31670da5ea4f5ca7dc4e65f6f342b95e4c
wkwkrnht <wkwkrnht@gmail.com>
2016-07-30 20:56:17 +0900

    fix

--- functions.php
+++ functions.php
@@ -271,7 +271,7 @@ function wkwkrnht_special_card(){
     $blogname=get_bloginfo('name');
     $sitedescription=get_bloginfo('description');
     if(is_author()===true):
-        $url = '.:' . get_template_directory() . '/widget/author-bio.php/';
+        $url = '.:' . get_template_directory() . '/widget/author-bio.php';
         include $url;
     else:
         echo'<div class="card info-card">';
