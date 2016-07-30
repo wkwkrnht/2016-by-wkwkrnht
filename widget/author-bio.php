115ca7e5090c346c40e2d0d29ce10672f50bf566
wkwkrnht <wkwkrnht@gmail.com>
2016-07-30 21:17:51 +0900

    fix

--- widget/author-bio.php
+++ widget/author-bio.php
@@ -2,8 +2,9 @@
     .bio-wrapper{display:block;}
 </style>
 <a href="<?php echo site_url() . '?author=' . get_the_author_meta('ID');?>" style="" class="bio-wrapper card info-card">
-    <header class="bio-header"><?php echo get_avatar(get_the_author_meta('ID'),108,no_image(),'avatar');?><span class="bio-name"><?php the_author();?></span></header>
+    <?php echo get_avatar(get_the_author_meta('ID'),256);?>
     <div class="bio-main">
+        <span class="bio-name"><?php the_author();?></span><br>
         <div class="follow-button"></div>
         <p class="bio-description"><?php the_author_meta('user_description');?></p>
     </div>
