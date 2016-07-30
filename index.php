c2884be7533771c9e66a43532b3309ae67a0fde2
wkwkrnht <wkwkrnht@gmail.com>
2016-07-30 13:52:13 +0900

    fix

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		        <a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>" class="article-eye"><img src="<?php wkwkrnht_eyecatch();?>" alt="eyecatch"></a>
 		        <div class="card-info">
 		            <a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>" class="card-eyecatch"><h2 class="article-name"><?php the_title();?></h2></a><br>
-		            <span class="card-meta">公開日：<time class="entry-date" datetime="<?php echo get_mtime('Y/n/j G:i.s');?>"><?php the_time('Y/n/j');?></time><br><?php echo'著者：<a href="'/* . get_the_author_link()*/ . '">';the_author();echo'</a><br>カテゴリー:';the_category(', ');?></span>
+		            <span class="card-meta">公開日：<time class="entry-date" datetime="<?php echo get_mtime('Y/n/j G:i.s');?>"><?php the_time('Y/n/j');?></time><br><?php echo'著者：<a href="' . site_url() . '?author=' . get_the_author_meta('id') . '">';the_author();echo'</a><br>カテゴリー:';the_category(', ');?></span>
 		        </div>
 		    </section>
 		<?php endwhile;endif;?>
