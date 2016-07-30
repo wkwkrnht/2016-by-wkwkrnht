ac64678f28d7f8fe90a589c77a352ca54181c6cd
wkwkrnht <wkwkrnht@gmail.com>
2016-07-30 22:07:38 +0900

    fix

--- functions.php
+++ functions.php
@@ -149,6 +149,22 @@ function get_meta_description_from_tag(){
     return $tag_desc;
 }
 
+add_filter( 'wp_title', 'wpdocs_hack_wp_title_for_home' );
+
+/**
+ * Customize the title for the home page, if one is not set.
+ *
+ * @param string $title The original title.
+ * @return string The title to use.
+ */
+function wpdocs_hack_wp_title_for_home( $title )
+{
+  if ( empty( $title ) && ( is_home() || is_front_page() ) ) {
+    $title = __( 'Home', 'textdomain' ) . ' | ' . get_bloginfo( 'description' );
+  }
+  return $title;
+}
+
 function meta_description(){
     if(is_singular()===true&&has_excerpt()===true):
         the_excerpt();
