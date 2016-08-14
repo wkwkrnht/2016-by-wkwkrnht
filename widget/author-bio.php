1917bbc8fab86207067645a66858768bdd6e67a6
wkwkrnht <wkwkrnht@gmail.com>
2016-08-14 14:54:39 +0900

    fix

--- widget/author-bio.php
+++ widget/author-bio.php
@@ -11,13 +11,13 @@
         <h1 class="bio-name"><?php the_author_meta('display_name');?></h1><br>
         <p class="bio-description"><?php the_author_meta('user_description');?></p>
         <ul class="follow-button">
-            <li><?php $tw = '';$tw = get_the_author_meta('twitter');if($tw!==''){echo $tw;}?></li>
-            <li><?php $fb = '';$fb = get_the_author_meta('facebook');if($fb!==''){echo $fb;}?></li>
-            <li><?php $gp = '';$gp = get_the_author_meta('Googleplus');if($gp!==''){echo $gp;}?></li>
-            <li><?php $ld = '';$ld = get_the_author_meta('Linkedin');if($ld!==''){echo $ld;}?></li>
+            <li><?php $tw = '';$tw = get_the_author_meta('twitter');if($tw!==''){echo'<a href="https://twitter.com/' . $tw . '"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>';}?></li>
+            <li><?php $fb = '';$fb = get_the_author_meta('facebook');if($fb!==''){echo'<a href="https://www.facebook.com/profile.php?id=' . $fb . '"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>';}?></li>
+            <li><?php $gp = '';$gp = get_the_author_meta('Googleplus');if($gp!==''){echo'<a href="https://plus.google.com/u/0/' . $gp . '"><i class="fa fa-google-plus-official" aria-hidden="true"></i></a>';}?></li>
+            <li><?php $ld = '';$ld = get_the_author_meta('Linkedin');if($ld!==''){echo'<a href="https://jp.linkedin.com/in/' . $ld . '"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>';}?></li>
             <li><?php $line = '';$line = get_the_author_meta('LINE');if($line!==''){echo $line;}?></li>
             <li><?php $Quita = '';$Quita = get_the_author_meta('Quita');if($Quita!==''){echo $Quita;}?></li>
-            <li><?php $Github = '';$Github = get_the_author_meta('Github');if($Github!==''){echo $Github;}?></li>
+            <li><a href="https://github.com/<?php $Github = '';$Github = get_the_author_meta('Github');if($Github!==''){echo $Github;}?>"></a></li>
         </ul>
     </div>
 </a>
