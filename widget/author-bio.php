98359a99d4a8e9855962537285c38532a964a98f
wkwkrnht <wkwkrnht@gmail.com>
2016-08-14 17:45:30 +0900

    fix

--- widget/author-bio.php
+++ widget/author-bio.php
@@ -12,13 +12,13 @@
             <p class="bio-description"><?php the_author_meta('user_description');?></p>
         </a>
         <ul class="follow-button">
-            <li><?php $tw = '';$tw = get_the_author_meta('twitter');if($tw!==''){echo'<a href="https://twitter.com/' . $tw . '"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>';}?></li>
-            <li><?php $fb = '';$fb = get_the_author_meta('facebook');if($fb!==''){echo'<a href="https://www.facebook.com/profile.php?id=' . $fb . '"><i class="fa fa-facebook-official fa-3x" aria-hidden="true"></i></a>';}?></li>
-            <li><?php $gp = '';$gp = get_the_author_meta('Googleplus');if($gp!==''){echo'<a href="https://plus.google.com/u/0/' . $gp . '"><i class="fa fa-google-plus-official fa-3x" aria-hidden="true"></i></a>';}?></li>
-            <li><?php $ld = '';$ld = get_the_author_meta('Linkedin');if($ld!==''){echo'<a href="https://jp.linkedin.com/in/' . $ld . '"><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>';}?></li>
-            <li><?php $line = '';$line = get_the_author_meta('LINE');if($line!==''){echo $line;}?></li>
-            <li><?php $Quita = '';$Quita = get_the_author_meta('Quita');if($Quita!==''){echo'<a href="http://qiita.com/' . $Quita . '">' . $Quita . '</a>';}?></li>
-            <li><?php $Github = '';$Github = get_the_author_meta('Github');if($Github!==''){echo'<a href="https://github.com/' . $Github . '"><i class="fa fa-github fa-3x" aria-hidden="true"></i></a>';}?></li>
+            <?php $tw = '';$tw = get_the_author_meta('twitter');if($tw!==''){echo'<li><a href="https://twitter.com/' . $tw . '"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a></li>';}?>
+            <?php $fb = '';$fb = get_the_author_meta('facebook');if($fb!==''){echo'<li><a href="https://www.facebook.com/profile.php?id=' . $fb . '"><i class="fa fa-facebook-official fa-3x" aria-hidden="true"></i></a></li>';}?>
+            <?php $gp = '';$gp = get_the_author_meta('Googleplus');if($gp!==''){echo'<li><a href="https://plus.google.com/u/0/' . $gp . '"><i class="fa fa-google-plus-official fa-3x" aria-hidden="true"></i></a></li>';}?>
+            <?php $ld = '';$ld = get_the_author_meta('Linkedin');if($ld!==''){echo'<li><a href="https://jp.linkedin.com/in/' . $ld . '"><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a></li>';}?>
+            <?php $line = '';$line = get_the_author_meta('LINE');if($line!==''){echo'<li>' . $line . '</li>';}?>
+            <?php $Quita = '';$Quita = get_the_author_meta('Quita');if($Quita!==''){echo'<li><a href="http://qiita.com/' . $Quita . '">' . $Quita . '</a>';}?></li>
+            <?php $Github = '';$Github = get_the_author_meta('Github');if($Github!==''){echo'<li><a href="https://github.com/' . $Github . '"><i class="fa fa-github fa-3x" aria-hidden="true"></i></a></li>';}?>
         </ul>
     </div>
 </section>
