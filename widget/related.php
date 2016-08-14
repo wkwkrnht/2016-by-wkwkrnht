b635ab29ffb624c6b68565baa8348247f583a7ba
wkwkrnht <wkwkrnht@gmail.com>
2016-08-14 22:18:50 +0900

    fix

--- widget/related.php
+++ widget/related.php
@@ -8,7 +8,7 @@
 <div id="flex">
 	<?php $categories=get_the_category();$category_ID=array();foreach($categories as $category):array_push($category_ID,$category->cat_ID);endforeach;
 	if(have_posts()):while(have_posts()):the_post();$now = get_the_ID();endwhile;endif;$array=array('numberposts'=>6,'category'=>$category_ID,'orderby'=>'rand','post__not_in'=>array($now),'no_found_rows'=>true,'update_post_term_cache'=>false,'update_post_meta_cache'=>false);
-	$query = new WP_Query($args);
+	$query = new WP_Query($array);
 	if($query -> have_posts()):
 		while($query -> have_posts()):$query -> the_post();?>
 			<a href="<?php the_permalink()?>" title="<?php the_title_attribute();?>" class="related-wrapper">
@@ -20,7 +20,7 @@
 	<?php else:
 		wp_reset_postdata();
 		$array=array('numberposts'=>6,'orderby'=>'rand','post__not_in'=>array($now),'no_found_rows'=>true,'update_post_term_cache'=>false,'update_post_meta_cache'=>false);
-		$query = new WP_Query($args);
+		$query = new WP_Query($array);
 		while($query -> have_posts()):$query -> the_post();?>
 			<a href="<?php the_permalink()?>" title="<?php the_title_attribute();?>" class="related-wrapper">
 				<img src="<?php wkwkrnht_eyecatch();?>" alt="thumbnail" class="related-thumb">
