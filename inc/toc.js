0bc7509e204effb2ac5b9631af48da1040ddb3da
wkwkrnht <wkwkrnht@gmail.com>
2016-08-17 21:30:25 +0900

    fix

--- inc/toc.js
+++ inc/toc.js
@@ -1,7 +1,6 @@
 jQuery('#outline .link').click(function(){
     var target = jQuery('*data-outline="' + jQuery(this).attr('href') + '"');// 移動先のオブジェクトを取得します
     if(target.length == 0){return false;}
-    jQuery('#article-main').animate({'scrollTop' : $target.offset().top},100);// 移動先までスクロールさせます
+    jQuery('html,body,main').animate({'scrollTop' : $target.offset().top},100);// 移動先までスクロールさせます
     return false;// 元のaタグを実行させないように、falseを返します
-  });
-})
+});
