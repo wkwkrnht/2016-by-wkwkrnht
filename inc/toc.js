60ba722da4bb1560590353b52ba46489903b11fa
wkwkrnht <wkwkrnht@gmail.com>
2016-08-17 21:01:15 +0900

    fix

--- inc/toc.js
+++ inc/toc.js
@@ -1,16 +1,7 @@
-$('#outline ul a').click(function(){
-    // 移動先のオブジェクトを取得します。
-    var $target = $('*[data-outline="' + $(this).attr('href') + '"]');
-    if ($target.length == 0) {
-      // 移動先を取得できなかった場合は処理を終了します。
-      // ※正しく目次を作成できている場合は、ここには入らないはず。
-      return false;
-    }
-
-    // 移動先までスクロールさせます。
-    $('html, body').animate({'scrollTop': $target.offset().top},100);
-
-    // 元のaタグを実行させないように、falseを返します。
-    return false;
+$('#outline .link').click(function(){
+    var target = $('*data-outline="' + $(this).attr('href') + '"');// 移動先のオブジェクトを取得します
+    if(target.length == 0){return false;}
+    $('html, body').animate({'scrollTop' : $target.offset().top},100);// 移動先までスクロールさせます
+    return false;// 元のaタグを実行させないように、falseを返します
   });
 })
