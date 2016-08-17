cb153b71cadb95b534653f1f4a192519fce6673b
wkwkrnht <wkwkrnht@gmail.com>
2016-08-17 22:20:44 +0900

    fix

--- inc/toc.js
+++ inc/toc.js
@@ -1,6 +1,6 @@
 jQuery('#outline ul a').click(function(){
     var target = jQuery('*[data-outline="' + jQuery(this).attr('href') + '"]');// 移動先のオブジェクトを取得します
     if(target.length == 0){return false;}
-    jQuery('html, body').animate({'scrollTop' : $target.offset().top},100);// 移動先までスクロールさせます
+    jQuery('html, body').animate({'scrollTop' : target.offset().top},100);// 移動先までスクロールさせます
     return false;// 元のaタグを実行させないように、falseを返します
 });
