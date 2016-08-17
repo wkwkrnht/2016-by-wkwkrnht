4b2a16a2777e699569957a6999fe62a9dd5211eb
wkwkrnht <wkwkrnht@gmail.com>
2016-08-17 21:25:23 +0900

    fix

--- inc/toc.js
+++ inc/toc.js
@@ -1,7 +1,7 @@
-$('#outline .link').click(function(){
-    var target = $('*data-outline="' + $(this).attr('href') + '"');// 移動先のオブジェクトを取得します
+jQuery('#outline .link').click(function(){
+    var target = jQuery('*data-outline="' + jQuery(this).attr('href') + '"');// 移動先のオブジェクトを取得します
     if(target.length == 0){return false;}
-    $('html, body').animate({'scrollTop' : $target.offset().top},100);// 移動先までスクロールさせます
+    jQuery('#article-main').animate({'scrollTop' : $target.offset().top},100);// 移動先までスクロールさせます
     return false;// 元のaタグを実行させないように、falseを返します
   });
 })
