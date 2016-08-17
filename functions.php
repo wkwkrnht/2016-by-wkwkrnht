5652db83972f517b27840463f8b9c24640e0a418
wkwkrnht <wkwkrnht@gmail.com>
2016-08-17 22:53:17 +0900

    test

--- functions.php
+++ functions.php
@@ -376,74 +376,22 @@ function wkwkrnht_page_navi(){
 	}
 	wp_reset_query();
 }
-
-function get_outline_info($content){
-    $outline = '';
-    if(preg_match_all('/<h([1-6])>(.*?)<\/h\1>/',$content,$matches,PREG_SET_ORDER)){
-        // 記事内で使われているh1〜h6タグの中の、1〜6の中の一番小さな数字を取得します
-        // ※以降ソースの中にある、levelという単語は1〜6のことを表します
-        $min_level = min(array_map(function($m){return $m[1];},$matches));
-        // スタート時のlevelを決定します
-        // ※このレベルが上がる毎に、<ul></li>タグが追加されていきます
-        $current_level = $min_level - 1;
-        $sub_levels = array('1' => 0, '2' => 0, '3' => 0, '4' => 0, '5' => 0, '6' => 0);// 各レベルの出現数を格納する配列を定義します
-        // 記事内で見つかった、hタグの数だけループします
-        foreach($matches as $m){
-            $level = $m[1];  // 見つかったhタグのlevelを取得します
-            $text = $m[2];  // 見つかったhタグの、タグの中身を取得します
-            // li, ulタグを閉じる処理です。2ループ目以降に中に入る可能性があります
-            // 例えば、前回処理したのがh3タグで、今回出現したのがh2タグの場合、h3タグ用のulを閉じて、h2タグに備えます
-            while($current_level > $level){
-                $current_level--;
-                $outline .= '</li></ul>';
-            }
-            //同じlevelならliタグを閉じ、新しく開きます。そうでない場合は、ul, liタグを追加していきます。(例えば、前回処理したのがh2タグで、今回出現したのがh3タグの場合、h3タグのためにulを追加します)
-            if($current_level===$level){
-                $outline .= '</li><li>';
-            }else{
-                while($current_level < $level){
-                    $current_level++;
-                    $outline .= sprintf('<ul class="indent_%s"><li>',$current_level);
-                }
-                for($idx = $current_level + 0; $idx < count($sub_levels); $idx++){$sub_levels[$idx] = 0;}// 見出しのレベルが変わった場合は、現在のレベル以下の出現回数をリセットします
-            }
-            $sub_levels[$current_level]++;// 各レベルの出現数を格納する配列を更新します
-            /*
-            現在処理中のhタグの、パスを入れる配列を定義します
-            例えば、h2 -> h3 -> h3タグと進んでいる場合は、
-            level_fullpathはarray(1, 2)のようになります。
-            ※level_fullpath[0]の1は、1番目のh2タグの直下に入っていることを表します。
-            ※level_fullpath[1]の2は、2番目のh3を表します
-            */
-            $level_fullpath = array();
-            for($idx = $min_level; $idx <= $level; $idx++){$level_fullpath[] = $sub_levels[$idx];}
-            $target_anchor = '#outline_' . implode('_',$level_fullpath);
-            // 目次に、<a href="#outline_1_2">1.2 見出し</a>のような形式で見出しを追加します。
-            $outline .= sprintf('<a href="%s" class="link">%s. %s</a>',$target_anchor,implode('.',$level_fullpath),$text);
-            // 本文中の見出し本体を、<h3>見出し</h3>を<h3 data-outline="#outline_1_2">見出し</h3>
-            // のような形式で置き換えます。
-            $content = preg_replace('/<h([1-6])>/','<h\1 data-outline="' . $target_anchor . '">',$content,1);
-        }
-        // hタグのループが終了後、閉じられていないulタグを閉じていきます。
-        while($current_level >= $min_level){
-            $outline .= '</li></ul>';
-            $current_level--;
-        }
-    }
-    return array('content'=>$content,'outline'=>$outline);
+function enque_toc_script(){
+    $script = "
+    <script src='" . get_stylesheet_directory_uri() . "/inc/toc.min.js'></script>
+    <script>
+    (function($){
+        $('#toc').toc({
+            'selectors': '.article-main h1','.article-main h2,.article-main h3','.article-main h4','.article-main h5','.article-main h6',
+            'anchorName': function(i, heading, prefix){
+                return prefix+i;
+            },
+        });
+    })(jQuery);
+    </script>
+    ";
+    add_filter('wp_footer',function($script){echo $script;})
 }
-function add_outline($content){
-    if(!is_singular()){return $content;}
-    $decorated_outline = '';
-    $shortcode_toc     = '[toc]';
-    $outline_info      = get_outline_info($content);
-    $content           = $outline_info['content'];
-    $outline           = $outline_info['outline'];
-    if($outline !== ''){$decorated_outline = '<section id="toc"><h2>目次</h2>' . $outline . '</section>';wp_enqueue_script('toc',get_stylesheet_directory_uri() . '/inc/toc.js',array(),false,true);}
-    if(strpos($content,$shortcode_toc) !== false){$content = str_replace($shortcode_toc,$decorated_outline,$content);}
-    return $content;
-}
-add_filter('the_content','add_outline');
 /*
     コンテンツ中装飾
 1.検索結果をマーカー風にハイライト
@@ -463,6 +411,7 @@ add_filter('comment_text','twtreplace');
 4.はてな版ブログカード
 5.検索風表示
 */
+function add_toc(){enque_toc_script();return '<div id="toc"></div>';}
 function style_into_article($atts){extract(shortcode_atts(array('style'=>'',),$atts));return'<pre class="wpcss" style="display:none;"><code>' . $style . '</code></pre>';}
 function html_encode($args=array(),$content=''){return htmlspecialchars($content,ENT_QUOTES,'UTF-8');}
 function wps_trend($atts){extract(shortcode_atts(array('width'=>'640','height'=>'480','geo'=>'JP','keyword'=>'','date'=>''),$atts));$height=(int)$height;$width=(int)$width;$keyword=esc_attr($keyword);$geo=esc_attr($geo);$date=esc_attr($date);return'<script src="//www.google.com/trends/embed.js?hl=ja&amp;q=' . $keyword . '&amp;geo=' . $geo . '&amp;date=' . $date . '&amp;cmpt=q&amp;content=1&amp;cid=TIMESERIES_GRAPH_0&amp;export=5&amp;w=' . $width . '&amp;h=' . $height . '"></script>';}
