3574a40a5eab8f9aa4beeecad7d5e47080f38a00
wkwkrnht <wkwkrnht@gmail.com>
2016-08-20 21:02:23 +0900

    fix

--- header.php
+++ header.php
@@ -28,22 +28,58 @@
 	<meta name="twitter:image" content="<?php meta_image();?>">
 	<meta name="twitter:site" content="@<?php echo get_option('Twitter_URL');?>">
 	<?php if(is_singular()===true):
-		$fb = '';
-		$tw = '';
-		$gp = '';
-		$fb = get_the_author_meta('facebook');
-		$tw = get_the_author_meta('twitter');
-		$gp = get_the_author_meta('Googleplus');
+		$fb   = '';
+		$tw   = '';
+		$gp   = '';
+		$logo = '';
+		$fb   = get_the_author_meta('facebook');
+		$tw   = get_the_author_meta('twitter');
+		$gp   = get_the_author_meta('Googleplus');
+		$logo = get_theme_mod('custom_logo');
 		if($fb!==''){echo'<meta property="article:author" content="' . $fb . '">';}
 		if($tw!==''){echo'<meta name="twitter:creator" content="' . $tw . '">';}
 		if($gp!==''){echo'<link rel="publisher" href="http://plus.google.com/' . $gp . '">';}
+		echo'<script type="application/ld+json">
+			{
+  				"@context": "http://schema.org",
+  				"@type": "NewsArticle",
+  				"mainEntityOfPage": {
+    				"@type": "WebPage",
+    				"@id": "https://google.com/article"
+  				},
+  				"headline": "Article headline",
+  					"image": {
+    				"@type": "ImageObject",
+    				"url": "' . get_meta_image() . '",
+    				"height": 256,
+    				"width": 256
+  				},
+  				"datePublished": "2015-02-05T08:00:00+08:00",
+  				"dateModified": "2015-02-05T09:20:00+08:00",
+  				"author": {
+    				"@type": "Person",
+    				"name": "' . get_the_author_meta('display_name') . '"
+  				},
+   				"publisher": {
+    				"@type": "Organization",
+    				"name": "' . get_bloginfo('name') . '",
+    				"logo": {
+      					"@type": "ImageObject",
+      					"url": "' . wp_get_attachment_url($logo) . '",
+      					"width": 256,
+      					"height": 256
+    				}
+  				},
+  				"description": "' . get_meta_description() . '"
+			}
+		</script>';
 	endif;?>
 	<link rel="amphtml" href="<?php echo get_permalink() . '?amp=1'; ?>">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo('pingback_url');?>">
 	<link rel="prerender" href="<?php if(is_home()):echo get_permalink();else:echo site_url();endif;?>">
 	<link rel="fluid-icon" href="<?php meta_image();?>" title="<?php bloginfo('name');?>">
-	<link rel="image_src" href="<?php meta_image();?>" url="<?php meta_image()?>" height="256" width="256">
+	<link rel="image_src" href="<?php meta_image();?>" url="<?php meta_image();?>" height="256" width="256">
 	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/style.css">
 	<?php $code = '';$code = get_option('Analytics');if($code!==''){echo $code;}?>
 	<?php wp_head();?>
