ed7fa6dea53319fa5183c4637f6df77bdfb3d51d
wkwkrnht <wkwkrnht@gmail.com>
2016-09-10 21:16:22 +0900

    fix

--- widget/related-post.php
+++ widget/related-post.php
@@ -4,7 +4,7 @@
 	.widget_related_posts .related-wrapper{display:block;height:20vw;width:28vw;border-radius:3vmin;padding:.5em 1em;margin:2vw;box-shadow:0 0 2vmin rgba(0,0,0,.3);background-color:#fff;color:#333;text-decoration:none;}
 	.widget_related_posts .related-wrapper:visited{color:#333;}
 	.widget_related_posts .related-title{box-shadow:0 3px 6px rgba(0,0,0,.1);background-color:#03a9f4;font-size:2rem;color:#fff;text-align:center;vertical-align:middle;}
-	.widget_related_posts .related-time,.widget_related_posts .related-category{font-size:1.6rem;text-align:left;}
+	.widget_related_posts .related-date,.widget_related_posts .related-category{font-size:1.6rem;text-align:left;}
 </style>
 <?php $categories=get_the_category();$category_ID=array();foreach($categories as $category):array_push($category_ID,$category->cat_ID);endforeach;
 if(have_posts()):while(have_posts()):the_post();$now = get_the_ID();endwhile;endif;$array=array('numberposts'=>10,'category'=>$category_ID,'orderby'=>'rand','post__not_in'=>array($now),'no_found_rows'=>true,'update_post_term_cache'=>false,'update_post_meta_cache'=>false);
