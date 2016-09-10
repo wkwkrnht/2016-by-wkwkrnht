0030d7c2483be55d80d352cabe4dfc5c6d560563
wkwkrnht <wkwkrnht@gmail.com>
2016-09-10 21:03:28 +0900

    Revert "fixx"
    This reverts commit acbc78af0d0848754b358fddf657e31702066f93.

--- header.php
+++ header.php
@@ -82,7 +82,9 @@
 	<link rel="prerender" href="<?php if(is_home()):echo get_permalink();else:echo site_url();endif;?>">
 	<link rel="fluid-icon" href="<?php meta_image();?>" title="<?php bloginfo('name');?>">
 	<link rel="image_src" href="<?php meta_image();?>" url="<?php meta_image();?>" height="256" width="256">
-	<?php echo fopen(get_stylesheet_directory_uri() . '/inc/styles.php','r+');?>
+	<style>
+		<?php echo file_get_contents(get_stylesheet_directory_uri() . "/inc/styles.css")."\n";?>
+	</style>
 	<?php $code = '';$code = get_option('Analytics');if($code!==''){echo $code;}?>
 	<?php wp_head();?>
 </head>
