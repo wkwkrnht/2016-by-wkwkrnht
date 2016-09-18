48426c2509b091cba6a730c308f5113abd2e57b9
wkwkrnht <wkwkrnht@gmail.com>
2016-09-18 23:04:50 +0900

    fix

--- widget/manth-archive.php
+++ widget/manth-archive.php
@@ -1,6 +1,6 @@
 <style>
     .widget_wkwkrnht_manth_archive ul{list-style:none;}
-    .widget_wkwkrnht_manth_archive .list-year{margin:1em auto;background-color:#03a9f4;border:1px solid #03a9f4;}
+    .widget_wkwkrnht_manth_archive .list-year{margin:1em 0;background-color:#03a9f4;border:1px solid #03a9f4;}
     .widget_wkwkrnht_manth_archive .list-year h3{text-align:center;}
     .widget_wkwkrnht_manth_archive .list-year h3 a{font-size:1.8rem;color:#fff;text-decoration:none;}
     .widget_wkwkrnht_manth_archive .article-list{list-style:none;padding:.75em 2em;margin-top:1em;background-color:#fff;display:flex;flex-wrap:wrap;justify-content:flex-start;align-items:center;}
@@ -19,7 +19,7 @@ echo'<ul>';
 foreach ($archives_year as $year_value){
     echo'<li class="list-year"><h3><a href="' . esc_url(home_url()) . '/' . ltrim($year_value) . '">' . ltrim($year_value) . '年</a></h3>';
     echo'<ul class="article-list">';
-    foreach($archives as $archives_value){if(intval(strip_tags($archives_value)) == intval($year_value)){echo  str_replace(intval($year_value).'年','',ltrim($archives_value));}}
+    foreach($archives as $archives_value){if(intval(strip_tags($archives_value)) == intval($year_value)){echo str_replace(intval($year_value) . '年','',ltrim($archives_value));}}
     echo'</ul></li>';
 }
 echo'</ul>';
