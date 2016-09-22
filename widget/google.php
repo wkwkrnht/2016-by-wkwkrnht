7567d4390a1145e04ba59ff8fa0dbad9c9c6f52e
wkwkrnht <wkwkrnht@gmail.com>
2016-09-23 00:59:29 +0900

    fix

--- widget/google.php
+++ widget/google.php
@@ -1,13 +1,13 @@
 <script>
-  (function(){
-    //'018423656682452105259:kmcdehafygu'
-    var cx = '<?php echo get_option('Disqus_ID');?>';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-  })();
+    (function(){
+        var cx = '<?php echo get_option('Google_search_ID');?>';
+        var gcse = document.createElement('script');
+        gcse.type = 'text/javascript';
+        gcse.async = true;
+        gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(gcse, s);
+    })();
+  //'018423656682452105259:kmcdehafygu'
 </script>
 <gcse:search></gcse:search>
