655d91e0d444ad3449b6a2658cbd3e8c60c78024
wkwkrnht <wkwkrnht@gmail.com>
2016-09-23 23:32:05 +0900

    fix

--- footer.php
+++ footer.php
@@ -54,7 +54,8 @@
     <script>if((new Date()).getHours() >= 21 || (new Date()).getHours() < 6 ){document.body.className += " night-mode";}</script>
     <style>
         .night-mode,.night-mode #main-menu,.night-mode .card,.night-mode .card-list{color:#fff;background-color:#333;}
-        .night-mode .ogp-blogcard{background-color:#333;border-color#f1f1f1:}
+        .night-mode .ogp-blogcard{background-color:#333;border-color:#f1f1f1;}
+        .night-mode .page-nation{background-color:#333;}
         .night-mode .article-main .ogp-blogcard-title,.night-mode .article-main .ogp-blogcard-description,.night-mode .article-main .ogp-blogcard-site-name,.night-mode .article-main .ogp-blogcard-title:visited,.night-mode .article-main .ogp-blogcard-description:visited,.night-mode .article-main .ogp-blogcard-site-name:visited,.night-mode .article-main img::after{color:#fff;}
     </style>
 </body>
