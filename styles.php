da5c3464117b11e39459c174828e3ba5a5e11105
wkwkrnht <wkwkrnht@gmail.com>
2016-09-30 21:57:00 +0900

    fix

--- styles.php
+++ styles.php
@@ -21,7 +21,7 @@
 	.hide-nav-prev,.hide-nav-next{height:100%;width:10vw;position:fixed;top:0;z-index:5;}
 	.hide-nav-prev{left:0;}
 	.hide-nav-next{right:0;}
-	.hide-nav-prev a,.hide-nav-next a{display:table-cell;height:100vh;width:10vw;text-align:center;vertical-align:middle;color:<?php echo get_option('nav_a_color','#fff');?>;background-color:<?php echo get_option('nav_a_background','#03a9f4');?>;transition:transform 0.2s linear;}
+	.hide-nav-prev a,.hide-nav-next a{display:table-cell;height:100vh;width:10vw;font-size:4rem;text-align:center;vertical-align:middle;color:<?php echo get_option('nav_a_color','#fff');?>;background-color:<?php echo get_option('nav_a_background','#03a9f4');?>;transition:transform 0.2s linear;}
 	.hide-nav-prev a{transform:translateX(-10vw);}
 	.hide-nav-next a{transform:translateX(10vw);}
 	.hide-nav-prev:hover a,.hide-nav-next:hover a{transform:translate(0);}
