f288f63b1c31143c06213c3a0005d99fc89f59d4
wkwkrnht <wkwkrnht@gmail.com>
2016-10-05 22:48:18 +0900

    fix

--- styles.php
+++ styles.php
@@ -242,7 +242,7 @@
 	.article-main .ogp-blogcard img{display:inline-block;}
 	.article-main .ogp-blogcard-title,.article-main .ogp-blogcard-description,.article-main .ogp-blogcard-site-name{color:#333;}
 	.article-main .ogp-blogcard-title:visited,.article-main .ogp-blogcard-description:visited,.article-main .ogp-blogcard-site-name:visited{color:#333;}
-	.article-main .ogp-blogcard-title{background-color:transparent;box-shadow:none;}
+	.article-main .ogp-blogcard-title{border:none;}
 
 	.format-chat .article-main p{display:block;height:3em;width:60%;padding:1em;border:1px solid #777;border-radius:5vmin;margin-bottom:2em;font-size:1.8rem;vertical-align:middle;}
 	.format-chat .article-main p:nth-of-type(odd){float:left;clear:both;margin-left:3vmin;background-color:rgba(139,195,74,.6);}
