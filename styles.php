7beac3c531e0a9f4e3991f1d417b8366d009460f
wkwkrnht <wkwkrnht@gmail.com>
2016-10-28 22:34:37 +0900

    fix

--- styles.php
+++ styles.php
@@ -161,7 +161,7 @@
 	5.area for notice
 	6.move to search
 	*/
-	.card{min-height:40.5vmin;width:72vmin;margin:3vmin;padding:2vmin 3vmin;border-radius:3vmin;font-size:1.8rem;text-align:center;overflow-y:auto;background-color:#fff;box-shadow:0 0 3vmin rgba(0,0,0,.2);}
+	.card{min-height:40.5vmin;width:72vmin;margin:3vmin;padding:2vmin 3vmin;border-radius:3vmin;font-size:1.8rem;text-align:center;background-color:#fff;box-shadow:0 0 3vmin rgba(0,0,0,.2);}
 	.card-eyecatch{display:block;float:left;height:100%;width:calc(72vmin / 2 - 1vmin);vertical-align:middle;}
 	.card-title{display:inline-block;height:calc(40.5vmin / 10 * 3);margin:0 1em;}
 	.card-meta{display:inline-block;height:calc(40.5vmin / 10 * 7);margin:0 1em;}
