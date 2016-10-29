3612ccdaa9e83f08f00943fc120fd0e98aec775f
wkwkrnht <wkwkrnht@gmail.com>
2016-10-29 18:02:07 +0900

    fix

--- functions.php
+++ functions.php
@@ -84,6 +84,7 @@ function wkwkrnht_widgets_init(){
     register_sidebar(array('name'=>'List Footer','id'=>'listfooter','before_widget'=>'<section class="card"><div id="%1$s" class="widget %2$s">','after_widget'=>'</div></section>','before_title'=>'<h2 class="widget-title">','after_title' =>'</h2>',));
     register_sidebar(array('name'=>'List Under','id'=>'listunder','before_widget'=>'<li id="%1$s" class="widget %2$s">','after_widget'=>'</li>','before_title'=>'<h2 class="widget-title">','after_title' =>'</h2>',));
     register_sidebar(array('name'=>'404 Page','id'=>'404','before_widget'=>'<section class="card"><div id="%1$s" class="widget %2$s">','after_widget'=>'</div></section>','before_title'=>'<h2 class="widget-title">','after_title' =>'</h2>',));
+    register_widget('wkwkrnht_categorytag');
     register_widget('wkwkrnht_manth_archive');
     register_widget('related_posts');
     register_widget('related_posts_img');
