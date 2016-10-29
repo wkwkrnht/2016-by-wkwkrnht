4d9d1776a1f55fb54bb9fbf51956d8592fc7c288
wkwkrnht <wkwkrnht@gmail.com>
2016-10-29 18:15:34 +0900

    fix

--- functions.php
+++ functions.php
@@ -84,7 +84,6 @@ function wkwkrnht_widgets_init(){
     register_sidebar(array('name'=>'List Footer','id'=>'listfooter','before_widget'=>'<section class="card"><div id="%1$s" class="widget %2$s">','after_widget'=>'</div></section>','before_title'=>'<h2 class="widget-title">','after_title' =>'</h2>',));
     register_sidebar(array('name'=>'List Under','id'=>'listunder','before_widget'=>'<li id="%1$s" class="widget %2$s">','after_widget'=>'</li>','before_title'=>'<h2 class="widget-title">','after_title' =>'</h2>',));
     register_sidebar(array('name'=>'404 Page','id'=>'404','before_widget'=>'<section class="card"><div id="%1$s" class="widget %2$s">','after_widget'=>'</div></section>','before_title'=>'<h2 class="widget-title">','after_title' =>'</h2>',));
-    register_widget('wkwkrnht_categorytag');
     register_widget('wkwkrnht_manth_archive');
     register_widget('related_posts');
     register_widget('related_posts_img');
@@ -97,19 +96,6 @@ function wkwkrnht_widgets_init(){
     register_widget('google_search_ads_widget');
     register_widget('google_two_ads_widget');
     register_widget('move_top');
-}
-
-class wkwkrnht_categorytag extends WP_Widget{
-    function __construct(){parent::__construct('wkwkrnht_categorytag','カテゴリーとタグのみの検索',array());}
-    public function widget($args,$instance){echo $args['before_widget'];include_once(get_template_directory() . '/widget/wkwkrnht-categorytag.php');echo $args['after_widget'];}
-    public function form($instance){$title=!empty($instance['title']) ? $instance['title'] : '';?>
-		<p>
-		<label for="<?php echo $this->get_field_id('title');?>">title</label>
-		<input class="widefat" id="<?php echo $this->get_field_id('title');?>" name="<?php echo $this->get_field_name('title');?>" type="text" value="<?php echo esc_attr($title);?>">
-		</p>
-		<?php
-	}
-	public function update($new_instance,$old_instance){$instance=array();$instance['title']=(!empty($new_instance['title'])) ? strip_tags($new_instance['title']):'';return $instance;}
 }
 
 class wkwkrnht_manth_archive extends WP_Widget{
