39e0627fd6f1d6820f91f5de93d7e088a2d6bc40
wkwkrnht <wkwkrnht@gmail.com>
2016-11-03 18:38:02 +0900

    fix

--- styles.php
+++ styles.php
@@ -136,7 +136,7 @@
 
 	.page-nation,.page-nav{list-style:none;height:10vw;width:80vw;margin:5vmin auto;background-color:<?php echo get_option('page_nation_background','#fff');?>;box-shadow:0 0 3vmin rgba(0,0,0,.2);}
 	.page-nation{display:flex;flex-wrap:wrap;justify-content:center;align-items:center;}
-    .page-nation a,.page-nation .current,.page-nation li .dots,.page-nav a{padding:2vmin;margin:0 .5vmin;border:solid .5vmin <?php echo get_option('page_nation_border','#03a9f4');?>;font-weight:bold;}
+    .page-nation a,.page-nation .current,.page-nation li .dots{padding:2vmin;margin:0 .5vmin;border:solid .5vmin <?php echo get_option('page_nation_border','#03a9f4');?>;font-weight:bold;}
     .page-nation a,.page-nation li .dots{background-color:<?php echo get_option('page_nation_a_background','#fff');?>;color:<?php echo get_option('page_nation_a_color','#03a9f4');?>;font-size:1.3rem;text-decoration:none;}
     .page-nation a:hover,.page-nation .current{color:<?php echo get_option('page_nation_hover_color','#fff');?>;background-color:<?php echo get_option('page_nation_hover_background','#03a9f4');?>;}
 	.page-nav a{color:<?php echo get_option('page_nav_color','#03a9f4');?>;margin:0 1em;}
