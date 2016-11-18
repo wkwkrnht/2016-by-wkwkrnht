6e89d09c865814b662cec58e617bc3c4f727a5e2
wkwkrnht <wkwkrnht@gmail.com>
2016-11-18 22:22:30 +0900

    fix

--- widget/manth-archive.php
+++ widget/manth-archive.php
@@ -3,7 +3,7 @@
     .widget_wkwkrnht_manth_archive .list-year{margin:1em 0;}
     .widget_wkwkrnht_manth_archive .list-year h3{display:inline-block;width:calc(30% - 1vmin);text-align:center;background-color:<?php echo get_option('manth_archive_year_background','#03a9f4');?>;}
     .widget_wkwkrnht_manth_archive .list-year h3 a{font-size:1.8rem;color:<?php echo get_option('manth_archive_year_h3_color','#fff');?>;text-decoration:none;}
-    .widget_wkwkrnht_manth_archive .article-list{display:inline-block;width:70%;list-style:none;padding:.75em 2em;}
+    .widget_wkwkrnht_manth_archive .article-list{display:inline-block;box-sizing:border-box;width:70%;list-style:none;padding:.75em 2em;}
     .widget_wkwkrnht_manth_archive .article-list li{display:inline-block;font-size:1.6rem;height:2em;width:4.5em;text-align:center;}
     .widget_wkwkrnht_manth_archive .article-list li a{text-decoration:none;}
 </style>
