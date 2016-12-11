36978564c342e23b34f54f31ebb0977a412ba5e6
wkwkrnht <wkwkrnht@gmail.com>
2016-12-11 11:28:50 +0900

    fix

--- styles.php
+++ styles.php
@@ -383,7 +383,7 @@
 		10.original
 		11.codex
 		*/
-		.article-header{position:relative;margin-bottom:4vh;}
+		.article-header{position:absolute;margin-bottom:4vh;}
 		.article-eyecatch{max-width:100%;position:absolute;bottom:0;text-align:center;vertical-align:middle;}
 		.article-meta{width:100%;position:absolute;bottom:0;font-size:2rem;text-align:center;vertical-align:middle;background-image:linear-gradient(to top,<?php $color = color_to_rgb($colorcode = get_option('article_meta_background','#f1f1f1'));echo'rgba(' . $color["red"] . ',' . $color["green"] . ',' . $color["blue"] . ',.8)';?>,rgba(0,0,0,0));}
 		.article-title{font-size:2.5rem;}
