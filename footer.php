a2868263d63c5ea3e018d752a6a6aa6f7efeaf04
wkwkrnht <wkwkrnht@gmail.com>
2016-12-19 23:37:36 +0900

    fix

--- footer.php
+++ footer.php
@@ -55,10 +55,10 @@
     <script>
         (function(){if((new Date()).getHours() >= 21 || (new Date()).getHours() < 6 ){document.body.className += " night-mode";}})()
         (function(){
-            var elements = document.getElementsByClassName("twitter-tweet");
+            var targetElements = document.getElementsByClassName("twitter-tweet");
             for( var i = 0,l = elements.length; l > i; i++ ) {
-	            var element = elements[i] ;
-	            element.classList.add("tw-align-center");
+	            var targetElement = targetElements[i] ;
+	            targetElement.classList.add("tw-align-center");
             }
         })()
         (function(){
@@ -85,7 +85,7 @@
                 document.cookie = " " + key + "=" + escape(n) + ";expires=" + myDate.toGMTString();
             }
             var n = getCookie(key);
-            if(n == ""){
+            if(n === ""){
                 window.alert("このサイトでは、よりよいサイト運営のためにCookieを使用しています。そこでお預かりした情報は、各提携先と共有する場合があります。ご了承ください。");
             }
             n++;
