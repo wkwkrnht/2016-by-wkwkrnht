8f36f00e5c554eb1f6963294a61275646514278e
wkwkrnht <wkwkrnht@gmail.com>
2016-12-19 23:22:18 +0900

    fix

--- functions.php
+++ functions.php
@@ -775,7 +775,7 @@ function make_toc($atts){
                         for (var n = 0; n < targetElement.length; n++) {
                             idCounter++;
                             var targetElement = targetElements[n];
-                            if (targetElement.classList.contains("*") == false) {
+                            if (targetElement.getAttribute("class") === "") {
                                 targetElement.id = "toc" + idCounter;
                             }
                         }
